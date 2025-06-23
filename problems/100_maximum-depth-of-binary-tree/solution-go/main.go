82dde29e58db44843ec7e93c4626f17cbeb696a4
trbutler4 <trbiv@proton.me>
2025-06-22 19:18:41 -0500

    adding a bunch of problems

---
+++ problems/100_maximum-depth-of-binary-tree/solution-go/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+
+	return 0
+}
+
+func main() {
+
+}
